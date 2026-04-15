45cf2684dc6a53427ff1d6b07af5c99ca71ac3f7
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-15 11:34:52 -0400

    Added resource_manager.asm

---
+++ resource_manager.asm
@@ -0,0 +1,9 @@
+; // ==================================
+; // resource_manager.asm
+; // ----------------------------------
+; // Responsible for loading and unloading large
+; // assets such as textures.
+; // ==================================
+INCLUDE default_header.inc
+
+END
