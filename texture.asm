012bdf26e3368ce5c6f6522eef5a76557e13bfa1
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-15 11:47:31 -0400

    Implemented new_texture and free_texture

--- texture.asm
+++ texture.asm
@@ -20,11 +20,16 @@ init_texture PROC PUBLIC USES ecx esi, h:DWORD, w:DWORD, pPixels:DWORD
 	mov eax, ecx
 init_texture ENDP
 
-new_texture PROC PUBLIC USES ecx
+new_texture PROC PUBLIC USES ecx, h:DWORD, w:DWORD, pPixels:DWORD
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Texture
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_texture, h, w, pPixels
+
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_texture ENDP
 
 free_texture PROC PUBLIC 
+	INVOKE HeapFree, hHeap, 0, ecx
 	ret
 free_texture ENDP
 
