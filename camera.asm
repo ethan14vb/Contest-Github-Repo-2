08f8647943e8cf28618d2fe19a05162b6c418b4e
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-15 11:50:39 -0400

    Added header comments to camera.asm

--- camera.asm
+++ camera.asm
@@ -10,6 +10,13 @@ INCLUDE heap_functions.inc
 INCLUDE camera.inc
 
 .code
+; // ----------------------------------
+; // init_camera
+; // Initializes memory with the contents of a Camera
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 init_camera PROC PUBLIC USES ecx esi, x: DWORD, y: DWORD
 	mov esi, x
 	mov (Camera PTR [ecx]).x, esi
@@ -20,6 +27,10 @@ init_camera PROC PUBLIC USES ecx esi, x: DWORD, y: DWORD
 	ret
 init_camera ENDP
 
+; // ----------------------------------
+; // new_camera
+; // Reserves heap space for the Object with parameters calls the initializer method
+; // ----------------------------------
 new_camera PROC PUBLIC USES ecx, x: DWORD, y: DWORD
 	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Camera
 	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
@@ -28,6 +39,13 @@ new_camera PROC PUBLIC USES ecx, x: DWORD, y: DWORD
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_camera ENDP
 
+; // ----------------------------------
+; // free_camera
+; // Convenient method for freeing a Camera
+; //
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 free_camera PROC PUBLIC 
 	INVOKE HeapFree, hHeap, 0, ecx
 	ret
