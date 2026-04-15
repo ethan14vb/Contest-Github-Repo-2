09890793729d8c00f62768e4cd03b31f371079a8
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-15 11:45:56 -0400

    Implemented init_texture

--- texture.asm
+++ texture.asm
@@ -7,14 +7,25 @@
 INCLUDE default_header.inc
 INCLUDE heap_functions.inc
 INCLUDE texture.inc
+
 .code
-init_texture PROC PUBLIC USES ecx esi
-	ret
+init_texture PROC PUBLIC USES ecx esi, h:DWORD, w:DWORD, pPixels:DWORD
+	mov esi, h
+	mov (Texture PTR [ecx]).h, esi
+	mov esi, w
+	mov (Texture PTR [ecx]).w, esi
+	mov esi, pPixels
+	mov (Texture PTR [ecx]).pPixels, esi
+
+	mov eax, ecx
 init_texture ENDP
+
 new_texture PROC PUBLIC USES ecx
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_texture ENDP
+
 free_texture PROC PUBLIC 
 	ret
 free_texture ENDP
+
 END
