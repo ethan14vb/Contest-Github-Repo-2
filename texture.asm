11d8997f0b72050c9ad9111a96eab7626f63d986
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-15 11:49:26 -0400

    Added header comments to texture.asm

--- texture.asm
+++ texture.asm
@@ -9,6 +9,13 @@ INCLUDE heap_functions.inc
 INCLUDE texture.inc
 
 .code
+; // ----------------------------------
+; // init_texture
+; // Initializes memory with the contents of a Texture
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 init_texture PROC PUBLIC USES ecx esi, h:DWORD, w:DWORD, pPixels:DWORD
 	mov esi, h
 	mov (Texture PTR [ecx]).h, esi
@@ -20,6 +27,10 @@ init_texture PROC PUBLIC USES ecx esi, h:DWORD, w:DWORD, pPixels:DWORD
 	mov eax, ecx
 init_texture ENDP
 
+; // ----------------------------------
+; // new_texture
+; // Reserves heap space for the Object with parameters calls the initializer method
+; // ----------------------------------
 new_texture PROC PUBLIC USES ecx, h:DWORD, w:DWORD, pPixels:DWORD
 	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Texture
 	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
@@ -28,6 +39,13 @@ new_texture PROC PUBLIC USES ecx, h:DWORD, w:DWORD, pPixels:DWORD
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_texture ENDP
 
+; // ----------------------------------
+; // free_texture
+; // Convenient method for freeing a Texture
+; //
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 free_texture PROC PUBLIC 
 	INVOKE HeapFree, hHeap, 0, ecx
 	ret
