a6f37feef299612fb6cf477192907b20f676ea40
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-04-15 21:41:44 -0400

    Added Knight Game Object

---
+++ knight_game_object.asm
@@ -0,0 +1,13 @@
+; // ==================================
+; // knight_game_object.asm
+; // ----------------------------------
+; // The knight is a subclass of GameObject designed
+; // to handle all logic regarding knight units
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE game_object.inc
+INCLUDE heap_functions.inc
+INCLUDE knight_game_object.inc
+
+END 
