7bc48896bf988b8096413389db40ffe32cc1445c
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-17 13:55:36 -0400

    Added timer_component.asm

---
+++ timer_component.asm
@@ -0,0 +1,15 @@
+; // ==================================
+; // timer_component.asm
+; // ----------------------------------
+; // The TimerComponent is a countdown timer and is the simplest 
+; // way to handle time-based logic in the engine. When a timer 
+; // reaches the end of its wait_time, it will emit the timeout 
+; // signal.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE timer_component.inc
+
+.code
+
+END
