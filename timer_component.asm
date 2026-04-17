fd4cd174a8c0fa6d1665029cea6e762af73c1b57
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-17 14:03:21 -0400

    Added init_timer and new_timer wip methods to timer_component.asm

--- timer_component.asm
+++ timer_component.asm
@@ -11,5 +11,20 @@ INCLUDE default_header.inc
 INCLUDE timer_component.inc
 
 .code
+init_timer PROC PUBLIC USES ebx ecx edx esi edi, wait_time : REAL4, one_shot : DWORD, autostart : DWORD
+	mov eax, wait_time
+	mov eax, one_shot
+	mov eax, autostart
+
+	ret
+init_timer ENDP
+
+new_timer PROC PUBLIC USES ebx ecx edx esi edi, wait_time : REAL4, one_shot : DWORD, autostart : DWORD
+	mov eax, wait_time
+	mov eax, one_shot
+	mov eax, autostart
+	
+	ret
+new_timer ENDP
 
 END
