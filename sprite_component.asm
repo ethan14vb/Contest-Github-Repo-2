e7a8e834b8fd4a797b242bcb36383a82f4d48775
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-16 22:06:57 -0400

    Added header comments to sprite_component.asm

--- sprite_component.asm
+++ sprite_component.asm
@@ -8,10 +8,24 @@
 INCLUDE default_header.inc
 INCLUDE sprite_component.inc
 INCLUDE heap_functions.inc
+
 .code
+; // ----------------------------------
+; // init_sprite_component
+; // Initializes memory with the contents of a SpriteComponent
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 init_sprite_component PROC PUBLIC USES ebx esi
 	ret
 init_sprite_component ENDP
+
+; // ----------------------------------
+; // new_sprite_component
+; // Allocates memory for a SpriteComponent and then calls
+; // the initializer method on it.
+; // ----------------------------------
 new_sprite_component PROC PUBLIC USES ebx ecx esi
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_sprite_component ENDP
