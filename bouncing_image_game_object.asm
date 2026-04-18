27b6ad3d75c185762831d16535c80fd778e9fbd2
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-18 13:05:56 -0400

    Added header comments to bouncing_image_game_object.asm

--- bouncing_image_game_object.asm
+++ bouncing_image_game_object.asm
@@ -6,12 +6,29 @@
 INCLUDE default_header.inc
 INCLUDE heap_functions.inc
 INCLUDE bouncing_image_game_object.inc
+
 .code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
+; // ----------------------------------
+; // init_bouncing_image_game_object
+; // Initializes memory with the contents of a BouncingImageGameObject
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
 init_bouncing_image_game_object PROC PUBLIC USES ebx ecx edx esi edi, pTexture : DWORD
 	mov eax, pTexture
 	ret
 init_bouncing_image_game_object ENDP
 
+; // ----------------------------------
+; // new_bouncing_image_game_object
+; // Allocates memory for a BouncingImageGameObject and then calls
+; // the initializer method on it.
+; // ----------------------------------
 new_bouncing_image_game_object PROC PUBLIC USES ebx ecx edx esi edi, pTexture : DWORD
 	mov eax, pTexture
 	ret
