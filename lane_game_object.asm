b678324ed6044ac3220cb0cccd3eab87af50f1dd
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-04-18 18:24:49 -0400

    Added Lane attributes and new method

--- lane_game_object.asm
+++ lane_game_object.asm
@@ -34,4 +34,20 @@ init_lane_game_object PROC PUBLIC USES esi ebx edx
 	ret
 init_lane_game_object ENDP
 
+; // ----------------------------------
+; // new_lane_game_object
+; // Reserves heap space for the Object with parameters calls the initializer method
+; // ----------------------------------
+new_lane_game_object PROC PUBLIC USES ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF LaneGameObject
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_lane_game_object
+
+	ret ; // Return with the address of the memory block in HeapAlloc
+new_lane_game_object ENDP
+
+; // ********************************************
+; // Instance methods
+; // ********************************************
+
 END
