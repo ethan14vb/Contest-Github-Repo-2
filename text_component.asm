53d87fc99a8e46dc1e64f2295c1b27295805f442
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-20 19:05:33 -0400

    Implemented init_text_component

--- text_component.asm
+++ text_component.asm
@@ -9,17 +9,26 @@ INCLUDE text_component.inc
 INCLUDE heap_functions.inc
 
 .code
-init_text_component PROC pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
-	mov eax, pFontTexture
-	mov eax, charW
-	mov eax, charH
-	mov eax, spacing
-	mov eax, maxChars
-	mov eax, pText
+init_text_component PROC PUBLIC USES ebx ecx esi, pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
+	mov esi, pFontTexture
+	mov (TextComponent PTR [ecx]).pFontTexture, esi
+	mov esi, charW
+	mov (TextComponent PTR [ecx]).charW, esi
+	mov esi, charH
+	mov (TextComponent PTR [ecx]).charH, esi
+	mov esi, spacing
+	mov (TextComponent PTR [ecx]).spacing, esi
+	mov esi, maxChars
+	mov (TextComponent PTR [ecx]).textMaxLen, esi
+	mov esi, pText
+	mov (TextComponent PTR [ecx]).pText, esi
+
+	mov eax, ecx
+
 	ret
 init_text_component ENDP
 
-new_text_component PROC pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
+new_text_component PROC PUBLIC USES ebx ecx esi, pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
 	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TextComponent
 	mov ecx, eax; // Move the memory address to ecx so it can function as a "this" pointer
 	INVOKE init_text_component, pFontTexture, charW, charH, spacing, maxChars, pText
