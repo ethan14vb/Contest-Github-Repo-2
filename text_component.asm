1311802312d6280658ce8644d666e03983b089fb
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-20 18:57:15 -0400

    Added text_component.asm

---
+++ text_component.asm
@@ -0,0 +1,13 @@
+; // ==================================
+; // TextComponent
+; // ----------------------------------
+; // Defines how text should appear on the screen.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE text_component.inc
+INCLUDE heap_functions.inc
+
+.code
+
+END
