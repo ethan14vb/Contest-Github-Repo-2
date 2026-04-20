8a6604c5d522a3872b6e5ef78c096968e3c6cf6c
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-20 19:02:04 -0400

    Added some constructor methods to text_component.asm

--- text_component.asm
+++ text_component.asm
@@ -9,5 +9,22 @@ INCLUDE text_component.inc
 INCLUDE heap_functions.inc
 
 .code
+init_text_component PROC pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
+	mov eax, pFontTexture
+	mov eax, charW
+	mov eax, charH
+	mov eax, spacing
+	mov eax, maxChars
+	mov eax, pText
+	ret
+init_text_component ENDP
+
+new_text_component PROC pFontTexture : DWORD, charW : DWORD, charH : DWORD, spacing : DWORD, maxChars : DWORD, pText : DWORD
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TextComponent
+	mov ecx, eax; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_text_component, pFontTexture, charW, charH, spacing, maxChars, pText
+
+	ret ; // Return with the address of the memory block in HeapAlloc
+new_text_component ENDP
 
 END
