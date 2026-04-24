0fd65f1cce8f9b795b2ef2be23a611fd1a049dfb
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-24 10:04:25 -0400

    Implemented constructor functions in animator_component.asm

--- animator_component.asm
+++ animator_component.asm
@@ -10,8 +10,41 @@ INCLUDE animator_component.inc
 INCLUDE heap_functions.inc
 
 .code
-new_animator_component PROC
+init_animator_component PROC PUBLIC USES ecx esi, pSprite:DWORD, pAnimations:DWORD, animCount:DWORD
+	local pThis
+	mov pThis, ecx
+	; // Parent constructor
+	INVOKE init_component
+	mov (Component PTR [ecx]).componentType, ANIMATOR_COMPONENT_ID
+
+	; // Given data
+	mov esi, pSprite
+	mov (AnimatorComponent PTR [ecx]).pSprite, esi
+	mov esi, pAnimations
+	mov (AnimatorComponent PTR [ecx]).pAnimations, esi
+	mov esi, animCount
+	mov (AnimatorComponent PTR [ecx]).animCount, esi
+
+	; // internal setup
+	mov (AnimatorComponent PTR [ecx]).curAnimIndex, 0
+	mov (AnimatorComponent PTR [ecx]).curFrameIndex, 0
+	mov (AnimatorComponent PTR [ecx]).timeAccumulator, 0
+
+	lea ecx, (AnimatorComponent PTR [ecx]).animFinishedEvent
+	INVOKE init_event
+
+	mov ecx, pThis
+	mov eax, ecx
+
 	ret
+init_animator_component ENDP
+
+new_animator_component PROC PUBLIC USES ecx esi, pSprite : DWORD, pAnimations : DWORD, animCount : DWORD
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF AnimatorComponent
+	mov ecx, eax; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_animator_component, pSprite, pAnimations, animCount
+
+	ret; // Return with the address of the memory block in HeapAlloc
 new_animator_component ENDP
 
 END
