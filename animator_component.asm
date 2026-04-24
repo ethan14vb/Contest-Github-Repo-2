3ab40acfe3e542d7804ee63670458abd3abe593e
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-24 09:54:04 -0400

    Added animator_component.asm

---
+++ animator_component.asm
@@ -0,0 +1,17 @@
+; // ==================================
+; // AnimatorComponent
+; // ----------------------------------
+; // Connects to a sprite component and changes the sprite's visible
+; // cell based on internal animations.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE animator_component.inc
+INCLUDE heap_functions.inc
+
+.code
+new_animator_component PROC
+	ret
+new_animator_component ENDP
+
+END
