49b8262e28fcf077f724fe203c79e625bc7e55df
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-27 18:48:54 -0400

    Added P1 keyboard initialization

--- sprite_test_scene.asm
+++ sprite_test_scene.asm
@@ -78,6 +78,28 @@ bind_gp_sel    InputBinding <ACTION_SELECT, 1000h>
 ; // Initializes the virtual controllers with the correct bindings
 ; // ----------------------------------
 init_virtual_controllers PROC PUBLIC USES eax ecx
+	mov p1Controller.deviceID, P1_DEVICE
+	lea ecx, p1Controller.bindings
+	INVOKE init_unordered_vector, 5
+
+	.IF P1_DEVICE EQ DEVICE_KEYBOARD
+		.IF P1_LAYOUT EQ 1
+			; // Push WASD
+			INVOKE push_back, OFFSET bind_w_up
+			INVOKE push_back, OFFSET bind_s_down
+			INVOKE push_back, OFFSET bind_a_left
+			INVOKE push_back, OFFSET bind_d_right
+			INVOKE push_back, OFFSET bind_space_sel
+		.ELSE
+			; // Push ARROWS
+			INVOKE push_back, OFFSET bind_up_up
+			INVOKE push_back, OFFSET bind_dn_down
+			INVOKE push_back, OFFSET bind_lf_left
+			INVOKE push_back, OFFSET bind_rt_right
+			INVOKE push_back, OFFSET bind_ent_sel
+		.ENDIF
+	.ENDIF
+
 	ret
 init_virtual_controllers ENDP
 
