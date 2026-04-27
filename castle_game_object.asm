6e53c4fe5b0a589b206200ee1b77e44f404730e9
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-04-27 12:09:02 -0400

    Added Castle Game Object Framework

---
+++ castle_game_object.asm
@@ -0,0 +1,59 @@
+; // ==================================
+; // castle_game_object.asm
+; // ----------------------------------
+; // The castle is a subclass of GameObject
+; // that handles all logic for both castles
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE game_object.inc
+INCLUDE heap_functions.inc
+INCLUDE lane_game_object.inc
+INCLUDE transform_component.inc
+
+.data
+CASTLE_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET game_object_update, OFFSET game_object_exit, OFFSET free_game_object>
+
+.code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
+; // ----------------------------------
+; // init_lane_game_object
+; // Initializes memory with the contents of a LaneGameObject
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_castle_game_object PROC PUBLIC USES esi ebx edx
+		local pThis
+	mov pThis, ecx
+
+	; // Parent constructor
+	INVOKE init_game_object, 0
+	mov (GameObject PTR [ecx]).gameObjectType, CASTLE_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET CASTLE_GAMEOBJECT_VTABLE
+
+	mov ecx, pThis
+	mov eax, ecx
+	ret
+init_castle_game_object ENDP
+
+; // ----------------------------------
+; // new_castle_game_object
+; // Reserves heap space for the Object with parameters calls the initializer method
+; // ----------------------------------
+new_castle_game_object PROC PUBLIC USES ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF LaneGameObject
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_lane_game_object
+
+	ret ; // Return with the address of the memory block in HeapAlloc
+new_castle_game_object ENDP
+
+; // ********************************************
+; // Instance methods
+; // ********************************************
+
+END
