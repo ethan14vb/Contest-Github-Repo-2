b7c348666548cd716b6d3e79731c32307a5e1ca0
jozfeal <163903053+jozfeal@users.noreply.github.com>
2026-04-27 14:11:28 -0400

    Added framework for new Shop GameObject

---
+++ shop_game_object.asm
@@ -0,0 +1,61 @@
+; // ==================================
+; // shop_game_object.asm
+; // ----------------------------------
+; // The shop is a subclass of GameObject
+; // that handles all logic for both player shops
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE game_object.inc
+INCLUDE heap_functions.inc
+INCLUDE castle_game_object.inc
+INCLUDE lane_game_object.inc
+INCLUDE knight_game_object.inc
+INCLUDE transform_component.inc
+INCLUDE sprite_component.inc
+
+.data
+SHOP_GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET game_object_update, OFFSET game_object_exit, OFFSET free_game_object>
+
+.code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
+; // ----------------------------------
+; // init_shop_game_object
+; // Initializes memory with the contents of a ShopGameObject
+; // 
+; // Register Parameters: 
+; //	ecx - THIS pointer
+; // ----------------------------------
+init_shop_game_object PROC PUBLIC USES esi ebx edx
+		local pThis
+	mov pThis, ecx
+
+	; // Parent constructor
+	INVOKE init_game_object, 0
+	mov (GameObject PTR [ecx]).gameObjectType, SHOP_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET SHOP_GAMEOBJECT_VTABLE
+
+	mov ecx, pThis
+	mov eax, ecx
+	ret
+init_shop_game_object ENDP
+
+; // ----------------------------------
+; // new_shop_game_object
+; // Reserves heap space for the Object with parameters calls the initializer method
+; // ----------------------------------
+new_shop_game_object PROC PUBLIC USES ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF CastleGameObject
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_shop_game_object
+
+	ret ; // Return with the address of the memory block in HeapAlloc
+new_shop_game_object ENDP
+
+; // ********************************************
+; // Instance methods
+; // ********************************************
+END
