3ef57559182e6bd062aa938ab27832d2d0c27ff7
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-27 16:10:19 -0400

    Added player_cursor_update and vTable

--- player_cursor.asm
+++ player_cursor.asm
@@ -11,11 +11,19 @@ INCLUDE heap_functions.inc
 INCLUDE transform_component.inc
 INCLUDE rect_component.inc
 
+.data
+PLAYER_CURSOR_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET player_cursor_update, OFFSET game_object_exit, OFFSET free_game_object>
+
 .code
+; // ********************************************
+; // Constructor Methods
+; // ********************************************
+
 init_player_cursor PROC PUBLIC USES ecx esi edi, pVirtualController:DWORD, pShop : DWORD, pCardList : DWORD
 	; // Parent constructor
 	INVOKE init_game_object, 0
 	mov (GameObject PTR [ecx]).gameObjectType, PLAYER_CURSOR_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET PLAYER_CURSOR_VTABLE
 
 	; // Add components
 	INVOKE new_transform_component, 0, 0, 0FFFFFFFFh
@@ -43,5 +51,14 @@ new_player_cursor PROC PUBLIC USES ecx ebx edx esi edi, pVirtualController:DWORD
 
 	ret ; // Return with the address of the memory block in HeapAlloc
 new_player_cursor ENDP
+
+; // ********************************************
+; // Instance Methods
+; // ********************************************
+
+player_cursor_update PROC USES ebx ecx edx esi edi, deltaTime:REAL4
+	mov eax, deltaTime ; // Use deltaTime so MASM doesn't complain
+	ret
+player_cursor_update ENDP
 
 END
