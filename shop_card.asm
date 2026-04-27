ea17e27d35f45b11bd0e38c37dab94a7c67b7eda
Ethan <152923596+ethan14vb@users.noreply.github.com>
2026-04-27 15:57:34 -0400

    Implemented shop card initializer methods

--- shop_card.asm
+++ shop_card.asm
@@ -6,14 +6,35 @@
 ; // ==================================
 
 INCLUDE default_header.inc
+INCLUDE game_object.inc
+INCLUDE shop_card.inc
+INCLUDE heap_functions.inc
+INCLUDE transform_component.inc
+INCLUDE rect_component.inc
 
 .code
-init_shop_card PROC PUBLIC USES ecx esi edi
+init_shop_card PROC PUBLIC USES ecx esi edi, itemId:DWORD, cost:DWORD
+	; // Parent constructor
+	INVOKE init_game_object, 0
+	mov (GameObject PTR [ecx]).gameObjectType, SHOP_CARD_GAME_OBJECT_ID
+
+	; // Add components
+	INVOKE new_transform_component, 0, 0, 0FFFFFFFFh
+	INVOKE add_component, ecx, eax
+
+	INVOKE new_rect_component, 100, 50, 50, 50, 50, 0FFFFFFFFh
+	INVOKE add_component, ecx, eax
+
+	mov eax, ecx
 	ret
 init_shop_card ENDP
 
 new_shop_card PROC PUBLIC USES ecx ebx edx esi edi, itemId:DWORD, cost:DWORD
-	ret
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF ShopCard
+	mov ecx, eax ; // Move the memory address to ecx so it can function as a "this" pointer
+	INVOKE init_shop_card, itemId, cost
+
+	ret ; // Return with the address of the memory block in HeapAlloc
 new_shop_card ENDP
 
 END
